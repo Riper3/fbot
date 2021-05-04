67ee8607f9b59bf1b194465e0c8aaecf35d24246
Javier Gonzalez Llamas <lastoner4@gmail.com>
2021-05-04 18:20:05 +0100

    Storage data every 10 seconds

--- main.py
+++ main.py
@@ -2,13 +2,30 @@ from websocket import create_connection
 from src.fapi.functions import login, getSymbol
 import json
 import time
+from src.mysql.mysql import select, insert
+
+
 
 ws = login()
 
+symbols = select('SELECT symbol_name, symbol_id from symbols WHERE status = 1')
+
 while True:
 
-    symbol = getSymbol(ws, "EURUSD")
+    for symbol in symbols:
+        symboldata = json.loads(getSymbol(ws, symbol['symbol_name']))
 
-    print(symbol)
+        print(symboldata)
 
-    time.sleep(3)
+        response = symboldata['returnData']
+
+        data = (
+        symbol['symbol_id'],
+        response['bid'],
+        response['ask'],
+        )
+
+        insert('INSERT INTO price_history (symbol_id, bid, ask, date) VALUES (%s, %s, %s, CURRENT_TIMESTAMP())', data)
+        time.sleep(0.2)
+
+    time.sleep(10)
